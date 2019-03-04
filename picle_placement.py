91d0a6ef0459536daf8a78c480edf6fab2fb1efa
Sergio <sergio.ruiz-bonilla@durham.ac.uk>
2019-03-04 15:48:40 +0000

    WOI

--- picle_placement.py
+++ picle_placement.py
@@ -18,6 +18,7 @@ from swift_io import *
 import seagen
 import spipgen
 from scipy.interpolate import RectBivariateSpline
+from scipy.interpolate import interp1d
 
 R_earth = 6371000;
 ##################
@@ -25,10 +26,9 @@ R_earth = 6371000;
 def _eq_density(x, y, z, radii, densities):
     rho = np.zeros(x.shape[0])
     r = np.sqrt(x*x + y*y + z*z)
-    dr = radii[1] - radii[0]
+    f = interp1d(radii, densities, kind = 'linear')
     for i in range(x.shape[0]):
-        k = int(r[i]/dr)
-        rho[i] = densities[k] + (densities[k + 1] - densities[k])*(r[i] - k*dr)/dr 
+        rho[i] = f(r[i])
     return rho
 
 
@@ -119,9 +119,14 @@ plt.xlabel(r"$z$ $[R_{earth}]$")
 plt.ylabel(r"$z'$ $[R_{earth}]$")
 plt.show()
 
+plt.scatter(z_fine_r/R_earth, z_P_fine_r/z_fine_r, s = 0.1, label = 'delta z = 0.011 R_earth', c = 'green')
+plt.xlabel(r"$z$ $[R_{earth}]$")
+plt.ylabel(r"$z'/z$")
+plt.legend()
+plt.show()
 
-plt.scatter(z_fine_r/R_earth, z_P_fine_r/z_fine_r, s = 0.1, label = 'fine-r-grid and normal', c = 'green')
-plt.scatter(particles.z/R_earth, zP/particles.z, s = 0.1, label = 'fine-z-grid', c = 'red')
+plt.scatter(z_fine_r/R_earth, z_P_fine_r/z_fine_r, s = 0.1, label = 'delta z = 0.011 R_earth', c = 'green')
+plt.scatter(particles.z/R_earth, zP/particles.z, s = 0.1, label = 'delta z = 0.005 R_earth', c = 'red')
 plt.xlabel(r"$z$ $[R_{earth}]$")
 plt.ylabel(r"$z'/z$")
 plt.legend()
@@ -181,23 +186,33 @@ with h5py.File(filename, 'w') as f:
                     4*Rs*R_earth, swift_to_SI)  
 
 #######
-####### layer by layer
-    
-data = pd.read_csv("1layer_n200.csv", header=0)
+####### particle by particle 2
+# model densities
+data = pd.read_csv("1layer_n10k.csv", header=0)
 Rs = data.R[0]
 Ts = data['T'][0]
-dr = data.R[0] - data.R[1]
 
-rho = np.load("profile_n200.npy")
+rho = np.load("profile_parallel.npy")
+r_array = np.load('r_array.npy')
+z_array = np.load('z_array.npy')
 rho_grid = rho[-1,:,:]
-Re = (np.sum(rho_grid[:, 0] > 0) - 1)*dr
+Re = np.max(r_array[rho_grid[:,0] > 0])
 
-r = np.arange(0, rho_grid.shape[0]*dr, dr)
-z = np.arange(0, rho_grid.shape[1]*dr, dr)
-rho_model = RectBivariateSpline(r, z, rho_grid, kx = 1, ky = 1)
+rho_model = RectBivariateSpline(r_array, z_array, rho_grid, kx = 1, ky = 1)
 
+spipgen_plot.plotrho(rho[-1], r_array, z_array)
+"""
+# plot the model density
+rM = np.linspace(0, r_array[-1], 1000)
+zM = np.linspace(0, z_array[-1], 1000)
+X, Y = np.meshgrid(rM, zM)
+Z = rho_model.ev(X, Y)
+
+plt.contour(X, Y, Z, levels = np.arange(0, 8000, 500))
+plt.show()
+"""
 # Get equatorial profile
-radii = np.arange(0, Re, dr/100000)
+radii = np.arange(0, Re, Re/100000)
 densities = rho_model.ev(radii,0)
 
 # Generate seagen sphere
@@ -205,73 +220,98 @@ N = 10**5
 particles = seagen.GenSphere(N, radii[1:], densities[1:])
 
 rho_seagen = _eq_density(particles.x, particles.y, particles.z, radii, densities)
-#rho_seagen = particles.A1_rho
+#rho_seagen_2 = particles.A1_rho
 
 zP = np.zeros(particles.m.shape[0])
-zPP = np.zeros(particles.m.shape[0])
-
+#linear (for meeting)
 for i in range(particles.m.shape[0]):
     rc = np.sqrt(particles.x[i]**2 + particles.y[i]**2)
     rho_spherical = rho_seagen[i]
-    z = particles.z[i]
-    f = lambda z: rho_model.ev(rc, np.abs(z)) - rho_spherical
-    if z < 0:
-        zP[i] = _bisection(f, z, 0.)
+    rho_slice = rho_model.ev(np.ones((z_array.shape))*rc, z_array)
+    k = (rho_slice >= rho_spherical).sum() - 1
+    zP[i] = (z_array[k] - z_array[k + 1])*(rho_spherical - rho_slice[k])
+    zP[i] = zP[i]/(rho_slice[k] - rho_slice[k + 1]) + z_array[k]
+    zP[i] = zP[i]*np.sign(particles.z[i])
+
+#zPz grid ######## new method ###########################################
+zpz_grid = np.zeros((rho_grid.shape))
+
+# interpolate equatorial profile
+f = interp1d(radii, densities, kind = 'linear')
+
+#fill zpz grid
+for i in range(0, zpz_grid.shape[0]):
+    for j in range(1, zpz_grid.shape[1]):
+        rc = r_array[i]
+        z = z_array[j]
         
-    else:
-        zP[i] = _bisection(f, 0., z)
+        r = np.sqrt(rc*rc + z*z)
+        
+        if r > np.max(radii):
+            rho_spherical = 0
+        else:
+            rho_spherical = f(r)
+            
+        rho_slice = rho_model.ev(np.ones((z_array.shape))*rc, z_array)
+        k = (rho_slice >= rho_spherical).sum() - 1
+        
+        if k < z_array.shape[0] - 1:
+            zpz_grid[i, j] = (z_array[k] - z_array[k + 1])*(rho_spherical - rho_slice[k])
+            zpz_grid[i, j] = zpz_grid[i, j]/(rho_slice[k] - rho_slice[k + 1]) + z_array[k]
+            zpz_grid[i, j] = zpz_grid[i, j]/z
 
-for r in np.unique(particles.A1_r):      
-    mask = np.logical_and(particles.A1_r == r, np.abs(particles.z) >= 0.9*np.abs(particles.z[0])) 
-    slope = np.linalg.lstsq(particles.z[mask].reshape((-1,1)), zP[mask], rcond=None)[0][0]
-    mask = particles.A1_r == r
-    zPP[mask] = particles.z[mask]*slope
-    if r == np.unique(particles.A1_r)[1]:
-        mask = particles.A1_r == particles.A1_r[0]
-        zPP[mask] = particles.z[mask]*slope
+zpz_grid[:, 0] = zpz_grid[:, 3]
+zpz_grid[:, 1] = zpz_grid[:, 3]
+zpz_grid[:, 2] = zpz_grid[:, 3]
 
+zpz_grid[0,:] = zpz_grid[1,:]
+
+zP = np.zeros(particles.m.shape[0])
+
+zpz_model = RectBivariateSpline(r_array, z_array, zpz_grid, kx = 1, ky = 1)
+
+for i in range(particles.m.shape[0]):
+    rc = np.sqrt(particles.x[i]**2 + particles.y[i]**2)
+    z = particles.z[i]
+    
+    zP[i] = np.abs(zpz_model.ev(rc, np.abs(z))*z)*np.sign(z)
+
+z_fine_r = np.load('z_fine_r.npy')
+z_P_fine_r = np.load('zP_fine_r.npy')
+z_fine = np.load('z_array_fine.npy')
+z_P_fine = np.load('z_array_p_fine.npy')
 """
-mask = np.isfinite(zP)
-slope = np.linalg.lstsq(particles.z[mask].reshape((-1,1)), zP[mask], rcond=None)[0][0]
-#plt.scatter(particles.z, zP, s = 1, alpha = 0.15)
-plt.scatter(particles.z, zPP, s = 1, alpha = 1, color = 'green')
-plt.plot(particles.z, particles.z, c = 'r', linewidth = 0.1)
-plt.plot(particles.z, slope*particles.z, c = 'black', linewidth = 0.3)
-plt.axes().axhline(color = 'black')
+np.isnan(zP).sum()
+
+plt.scatter(z_fine_r/R_earth, z_P_fine_r/z_fine_r, s = 0.1, label = 'delta z = 0.011 R_earth', c = 'green')
+
+plt.scatter(particles.z/R_earth, zP/particles.z, s = 0.1, label = 'delta z = 0.005 R_earth', c = 'red')
 plt.xlabel(r"$z$ $[R_{earth}]$")
-plt.ylabel(r"$z'$ $[R_{earth}]$")
+plt.ylabel(r"$z'/z$")
+plt.legend()
+plt.show()
+
+plt.scatter(particles.z/R_earth, particles.z/zP, s =1)
 plt.show()
 """
-zPPP = np.zeros(particles.m.shape[0])
-for i in range(zP.shape[0]):
-    if np.isnan(zP[i]) or np.abs(zP[i]) < dr:
-        zPPP[i] = zPP[i]
-    else:
-        zPPP[i] = zP[i]
+mP = particles.m*zP/particles.z
 
-"""
-plt.scatter(particles.z, particles.z/zPP, s =1, color = 'red', alpha = 0.5)
-plt.scatter(particles.z, particles.z/zP, s =1, color = 'green', alpha = 0.5)
-plt.scatter(particles.z, particles.z/zPPP, s =1, color = 'blue', alpha = 0.5)
-plt.show()
-"""
+#plt.hist(mP, bins = 100)
+#plt.show()
 
-mP = particles.m*zPPP/particles.z
 # velocities
+
 Tw = 4 # in hours
 # v = w x r (units of R_earth/hour)  
 vx = np.zeros(mP.shape[0])
 vy = np.zeros(mP.shape[0])
 vz = np.zeros(mP.shape[0])
 
-wz = 2*np.pi/Tw 
+wz = 2*np.pi/Tw/hour_to_s 
 for i in range(mP.shape[0]):
     vx[i] = -particles.y[i]*wz
     vy[i] = particles.x[i]*wz
     
-vx = vx*R_earth/hour_to_s
-vy = vy*R_earth/hour_to_s
-
 # model densities and internal energy
 rho = np.zeros((mP.shape[0]))
 u = np.zeros((mP.shape[0]))
@@ -281,7 +321,7 @@ y = particles.y
 
 for k in range(mP.shape[0]):
     rc = np.sqrt(x[k]*x[k] + y[k]*y[k])
-    rho[k] = rho_model.ev(rc, zPPP[k])
+    rho[k] = rho_model.ev(rc, np.abs(zP[k]))
     u[k] = spipgen.ucold(rho[k], spipgen.granite, 10000) + spipgen.granite[11]*Ts
     
 ## Smoothing lengths, crudely estimated from the densities
@@ -293,16 +333,14 @@ A1_P = np.ones((mP.shape[0],))
 A1_id = np.arange(mP.shape[0])
 A1_mat_id = np.ones((mP.shape[0],))*Di_mat_id['Til_granite']
 
-swift_to_SI = Conversions(M_earth, R_earth, 1)
+swift_to_SI = Conversions(1, 1, 1)
 
 # save profile
-filename = 'init_test_linear3.hdf5'
+filename = 'init_test_linear.hdf5'
 with h5py.File(filename, 'w') as f:
-    save_picle_data(f, np.array([x, y, zPPP]).T, np.array([vx, vy, vz]).T,
+    save_picle_data(f, np.array([x, y, zP]).T, np.array([vx, vy, vz]).T,
                     mP, A1_h, rho, A1_P, u, A1_id, A1_mat_id,
-                    4*Rs, swift_to_SI) 
-
-
+                    4*Rs*R_earth, swift_to_SI)  
 #######
 ###### test: spherical case
 data = pd.read_csv("1layer_n10k.csv", header=0)
