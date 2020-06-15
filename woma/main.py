027890544643620084075bf426f608e0afd49aff
Jacob Kegerreis <jacob.kegerreis@durham.ac.uk>
2020-06-15 13:59:24 +0100

    Redo the ice giant example

--- woma/main.py
+++ woma/main.py
@@ -1247,7 +1247,7 @@ class Planet:
         assert self.A1_mat_id_layer[2] is not None
         assert self.A1_T_rho_type_id[2] is not None
 
-    def gen_prof_L3_find_R1_R2_given_R_M_I(self):
+    def gen_prof_L3_find_R1_R2_given_R_M_I(self):### WIP
         # Check for necessary input
         assert self.R is not None
         assert self.M is not None
@@ -1330,8 +1330,7 @@ class Planet:
         self.update_attributes()
         self.print_info()
 
-    def gen_prof_L3_find_R_R1_R2_given_M_M1_M2(self):
-        ###todo
+    def gen_prof_L3_find_R_R1_R2_given_M_M1_M2(self):### WIP
         return None
 
     def gen_prof_L3_find_R2_given_R_M_R1(self):
