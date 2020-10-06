16796e1ff3fb3a541567f5048b437427dfb9373a
Jacob Kegerreis <jacob.kegerreis@durham.ac.uk>
2020-10-06 20:21:58 +0100

    Add eos modules to init

--- woma/__init__.py
+++ woma/__init__.py
@@ -7,3 +7,4 @@ import woma.misc.glob_vars as glob_vars
 from woma.misc.utils import Conversions, impact_pos_vel_b_v_c_r, impact_pos_vel_b_v_c_t
 from woma.spin_funcs.utils_spin import rho_at_r_z
 from woma.misc.io import save_particle_data
+from woma.eos import T_rho, tillotson, sesame, idg, hm80
