093b4ba97b64fd6863cc3e7723510e7ff28851c2
Jacob Kegerreis <jacob.kegerreis@durham.ac.uk>
2021-01-26 18:48:58 +0000

    Add more useful things to __init__.py

--- woma/__init__.py
+++ woma/__init__.py
@@ -4,7 +4,9 @@ from woma.main import (
     ParticlePlanet,
 )
 import woma.misc.glob_vars as glob_vars
+from woma.misc.glob_vars import type_factor, Di_mat_type, Di_mat_id, Di_id_mat, Di_T_rho_id
 from woma.misc.utils import Conversions, impact_pos_vel_b_v_c_r, impact_pos_vel_b_v_c_t
 from woma.spin_funcs.utils_spin import rho_at_r_z
 from woma.misc.io import save_particle_data
 from woma.eos import T_rho, tillotson, sesame, idg, hm80
+from woma.eos.eos import P_u_rho, u_rho_T, P_T_rho, s_rho_T, rho_P_T
