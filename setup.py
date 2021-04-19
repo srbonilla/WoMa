465811ab305106383f498e493d15640e40aacaae
Sergio <sergio.ruiz-bonilla@durham.ac.uk>
2021-04-19 10:51:29 +0100

    Update version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 setuptools.setup(
     name="woma",
     packages=setuptools.find_packages(),
-    version="1.1.7",
+    version="1.1.8",
     description="WorldMaker",
     long_description=long_description,
     long_description_content_type="text/markdown",
