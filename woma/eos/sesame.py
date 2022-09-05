ef2f8bc52dd94b66723a1acb65e3e7c964c13eaf
Tom Sandnes <tdsandnes@gmail.com>
2022-09-05 17:36:05 +0100

    Ignore first row and column in rho_u_P

--- woma/eos/sesame.py
+++ woma/eos/sesame.py
@@ -2481,6 +2481,11 @@ def rho_u_P(u, P, mat_id, rho_ref):
             + "Use the woma.load_eos_tables function.\n"
         )
 
+    # Ignore the first elements of rho = 0, T = 0
+    A2_P = A2_P[1:, 1:]
+    A1_log_rho = A1_log_rho[1:]
+    A2_log_u = A2_log_u[1:, 1:]
+    
     # Convert inputs to log
     log_u = np.log(u)
     log_P = np.log(P)
