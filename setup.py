cdee942ee5f72513afc2b278bc8afc5947a30f24
jkeger <jacob.kegerreis@gmail.com>
2026-03-06 08:17:53 +0000

    Update version

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r") as f:
 setuptools.setup(
     name="woma",
     packages=setuptools.find_packages(),
-    version="1.4.4",
+    version="1.5.0",
     description="WorldMaker",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Sergio Ruiz-Bonilla, Jacob Kegerreis",
-    author_email="jacob.kegerreis@imperial.ac.uk",
+    author_email="j.kegerreis@imperial.ac.uk",
     url="https://github.com/srbonilla/WoMa",
     project_urls={
         "Paper": "https://arxiv.org/abs/2007.02965",
